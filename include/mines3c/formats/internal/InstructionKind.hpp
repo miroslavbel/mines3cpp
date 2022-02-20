a849b81eecb24385388f32aab8315ca889b6c436
Miroslav <miroslavbelov8@gmail.com>
2022-02-20 16:31:27 +0300

    [M3C-12] improve doc

--- include/mines3c/formats/internal/InstructionKind.hpp
+++ include/mines3c/formats/internal/InstructionKind.hpp
@@ -9,7 +9,9 @@ namespace internal {
  * \brief Классифицирует инструкции по характеру дополнительной информации ими
  * несомой.
  *
- * Каждая инструкция относится к одному из этих типов.
+ * Каждая инструкция относится ровно к одному из этих типов.
+ *
+ * \sa InstructionTypeId
  */
 enum class InstructionKind {
     /*!
@@ -19,32 +21,38 @@ enum class InstructionKind {
      */
     Simple,
     /*!
-     * \brief Инструкция содержит в себе литерал идентификатора метки.
+     * \brief Инструкция содержит в себе \link LabelIdentificatorLiteral литерал
+     * идентификатора метки\endlink.
      *
-     * \sa \link InstructionTypeId::TypeId::GOTO GOTO \endlink, \link
-     * InstructionTypeId::TypeId::GOSUB GOSUB \endlink, \link
-     * InstructionTypeId::TypeId::GOSUB1 GOSUB1 \endlink, \link
-     * InstructionTypeId::TypeId::LABEL LABEL \endlink, \link
-     * InstructionTypeId::TypeId::GOSUBF GOSUBF \endlink, \link
-     * InstructionTypeId::TypeId::IF_NOT_GOTO IF_NOT_GOTO \endlink, \link
-     * InstructionTypeId::TypeId::IF_GOTO IF_GOTO \endlink, \link
-     * InstructionTypeId::TypeId::ON_RESP ON_RESP \endlink
+     * \sa Полный список инструкций этого типа: \link
+     * InstructionTypeId::TypeId::GOTO GOTO\endlink, \link
+     * InstructionTypeId::TypeId::GOSUB GOSUB\endlink, \link
+     * InstructionTypeId::TypeId::GOSUB1 GOSUB1\endlink, \link
+     * InstructionTypeId::TypeId::LABEL LABEL\endlink, \link
+     * InstructionTypeId::TypeId::GOSUBF GOSUBF\endlink, \link
+     * InstructionTypeId::TypeId::IF_NOT_GOTO IF_NOT_GOTO\endlink, \link
+     * InstructionTypeId::TypeId::IF_GOTO IF_GOTO\endlink, \link
+     * InstructionTypeId::TypeId::ON_RESP ON_RESP\endlink.
      */
     Label,
     /*!
-     * \brief Инструкция содержит в себе литерал идентификатора переменной и
-     * литерал значения переменной.
+     * \brief Инструкция содержит в себе \link VariableIdentificatorLiteral
+     * литерал идентификатора переменной\endlink и \link VariableValueLiteral
+     * литерал значения переменной\endlink.
      *
-     * \sa \link InstructionTypeId::TypeId::VAR_MORE VAR_MORE \endlink, \link
-     * InstructionTypeId::TypeId::VAR_LESS VAR_LESS \endlink, \link
-     * InstructionTypeId::TypeId::VAR_EQUAL VAR_EQUAL \endlink
+     * \sa Полный список инструкций этого типа: \link
+     * InstructionTypeId::TypeId::VAR_MORE VAR_MORE\endlink, \link
+     * InstructionTypeId::TypeId::VAR_LESS VAR_LESS\endlink, \link
+     * InstructionTypeId::TypeId::VAR_EQUAL VAR_EQUAL\endlink.
      */
     VarCmp,
     /*!
-     * \brief Инструкция содержит в себе литерал строки.
+     * \brief Инструкция содержит в себе \link StringLiteral литерал
+     * строки\endlink.
      *
-     * \sa \link InstructionTypeId::TypeId::DEBUG_BREAK DEBUG_BREAK \endlink,
-     * \link InstructionTypeId::TypeId::DEBUG_SET DEBUG_SET \endlink
+     * \sa Полный список инструкций этого типа: \link
+     * InstructionTypeId::TypeId::DEBUG_BREAK DEBUG_BREAK\endlink, \link
+     * InstructionTypeId::TypeId::DEBUG_SET DEBUG_SET\endlink.
      */
     String,
 };
